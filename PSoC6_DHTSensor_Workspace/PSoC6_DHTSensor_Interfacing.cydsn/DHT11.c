8714e38bc1dfd6fc542ea021d5961463b97c38fc
Dheeraj Kamath <Dheeraj22@users.noreply.github.com>
2019-09-15 13:25:48 +0530

    Update DHT11.c

--- PSoC6_DHTSensor_Workspace/PSoC6_DHTSensor_Interfacing.cydsn/DHT11.c
+++ PSoC6_DHTSensor_Workspace/PSoC6_DHTSensor_Interfacing.cydsn/DHT11.c
@@ -1,11 +1,6 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * Author: Dheeraj D Kamath
  *
  * ========================================
 */
